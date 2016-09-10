0aef4c5445d2b1d8c9808f2cc1c1f5e1ff5bd15e
danielfoxp2 <daniel.foxp2@live.com>
2016-09-10 18:43:03 +0000

    Implementação de passo de verificação de país finalizada

--- password/features/step_definitions/pesquisa_cep_steps.rb
+++ password/features/step_definitions/pesquisa_cep_steps.rb
@@ -1,9 +1,10 @@
 Quando(/^pesquiso pelo CEP "([^"]*)"$/) do |numero_do_cep|
-  @informacoes_do_cep = CEP.dados_completos(numero_do_cep)
+  @informacoes_do_cep ||= {}
+  @informacoes_do_cep = BuscaRegiao.do_cep(numero_do_cep)
 end
 
-Então(/^vejo que o CEP pertence ao país "([^"]*)"$/) do |arg1|
-    pending # Write code here that turns the phrase above into concrete actions
+Então(/^vejo que o CEP pertence ao país "([^"]*)"$/) do |pais|
+  expect(@informacoes_do_cep.fetch(:pais)).to eq(pais)
 end
 
 Então(/^vejo que o CEP pertence ao estado "([^"]*)"$/) do |arg1|
