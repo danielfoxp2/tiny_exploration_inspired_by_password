3d6619f34c6ee1a3aee79938b0e95f0b1a8f2b5d
danielfoxp2 <daniel.foxp2@live.com>
2016-09-14 22:59:50 +0000

    Configuração para execução de specs no rails (controller)

---
+++ password/app/controllers/cep_controller.rb
@@ -0,0 +1,3 @@
+class CepController < ActionController::Base
+
+end
---
+++ password/spec/controllers/cep_controller_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe CepController, type: :controller do
+  it "should fail" do
+    expect(1).to eq(2)
+  end
+end
